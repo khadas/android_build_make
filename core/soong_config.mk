90db3203e2bfceb8644943cf02d657e1b8665324
Jian Qiu <qiujian@rock-chips.com>
2023-08-10 16:42:35 +0800

    add CameraSupportVirtual variable
    Signed-off-by: Jian Qiu <qiujian@rock-chips.com>
    Change-Id: I29106c1cdeadad40d846e61c5d120e8e400523b5

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -242,6 +242,7 @@ $(call add_json_str,  DexpreoptGlobalConfig,             $(DEX_PREOPT_CONFIG))
 $(call add_json_bool, WithDexpreopt,                     $(filter true,$(WITH_DEXPREOPT)))
 
 $(call add_json_bool, CameraSupportHDMI, 		 $(filter true,$(CAMERA_SUPPORT_HDMI)))
+$(call add_json_bool, CameraSupportVirtual, 		 $(filter true,$(CAMERA_SUPPORT_VIRTUAL)))
 
 $(call add_json_list, ManifestPackageNameOverrides,      $(PRODUCT_MANIFEST_PACKAGE_NAME_OVERRIDES))
 $(call add_json_list, PackageNameOverrides,              $(PRODUCT_PACKAGE_NAME_OVERRIDES))
