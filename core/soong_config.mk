meta-vi/rockchip-android-build

add CameraSupportHDMISubVideo variable
Signed-off-by: Jian Qiu <qiujian@rock-chips.com>
Change-Id: I659d303cd1ea32b4e53beac9beb549526ffc6065

core/soong_config.mk
@@ -207,6 +207,7 @@ $(call add_json_str,  DexpreoptGlobalConfig,             $(DEX_PREOPT_CONFIG))
 $(call add_json_bool, WithDexpreopt,                     $(filter true,$(WITH_DEXPREOPT)))
 
 $(call add_json_bool, CameraSupportHDMI, 		 $(filter true,$(CAMERA_SUPPORT_HDMI)))
+$(call add_json_bool, CameraSupportHDMISubVideo, 	 $(filter true,$(CAMERA_SUPPORT_HDMI_SUBVIDEO)))
 $(call add_json_bool, CameraSupportOSD, 		 $(filter true,$(CAMERA_SUPPORT_OSD)))
 
 $(call add_json_list, ManifestPackageNameOverrides,      $(PRODUCT_MANIFEST_PACKAGE_NAME_OVERRIDES))

khadas/android_build_make

core/soong_config.mk
@@ -241,6 +241,8 @@ $(call add_json_str,  DexpreoptGlobalConfig,             $(DEX_PREOPT_CONFIG))
 
 $(call add_json_bool, WithDexpreopt,                     $(filter true,$(WITH_DEXPREOPT)))
 
+$(call add_json_bool, CameraSupportHDMI, 		 $(filter true,$(CAMERA_SUPPORT_HDMI)))
+
 $(call add_json_list, ManifestPackageNameOverrides,      $(PRODUCT_MANIFEST_PACKAGE_NAME_OVERRIDES))
 $(call add_json_list, PackageNameOverrides,              $(PRODUCT_PACKAGE_NAME_OVERRIDES))
 $(call add_json_list, CertificateOverrides,              $(PRODUCT_CERTIFICATE_OVERRIDES))